855d6b0257f9ffec5be7e3a42b19d55348052edb
diorm47 <dior47m@gmail.com>
2023-10-20 02:14:15 +0500

    some edits

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -14,14 +14,25 @@ $(document).ready(function () {
 // Header bg carousel
 const bg1 = document.getElementById("bg1");
 const bg2 = document.getElementById("bg2");
+
 const images = [
   "url(../assets/images/header-bg.png)",
-  "url(../assets/images/header-bg-2.jpg)",
+  "url(../assets/images/header-bg-2.jpg)"
+];
+const imagesMob = [
+  "url(../assets/images/header-bg.png)",
+  "url(../assets/images/header-mob.jpg)"
 ];
 let i = 0;
 
+function getImagesArray() {
+  return window.innerWidth < 500 ? imagesMob : images;
+}
+
 setInterval(() => {
-  const nextImage = images[i % images.length];
+  const currentImages = getImagesArray();
+  const nextImage = currentImages[i % currentImages.length];
+
   if (bg1.style.opacity === "1") {
     bg2.style.backgroundImage = nextImage;
     bg1.style.opacity = "0";
@@ -31,7 +42,6 @@ setInterval(() => {
     bg1.style.opacity = "1";
     bg2.style.opacity = "0";
   }
+
   i++;
 }, 5000);
-
-
